0c1e5b205017229c6c28ce7d9705ab824cf59a2c
Ilan Yaniv <ilanyaniv@Ilans-MacBook-Pro.local>
2020-02-27 13:12:15 +0200

    removed debugging code

--- src/app.js
+++ src/app.js
@@ -1,29 +1,5 @@
 const child_process = require("child_process")
 
-var workspace_params = {
-    workspaceName: 'ilan',
-    URL: 'http://localhost:8080/tfs/DefaultCollection'
-}
-/*tf label labelname[@scope] [/owner:ownername] 
-itemspec [/version:versionspec] [/comment:("comment"|@commentfile)] 
-[/child:(replace|merge)] [/recursive] [/login:username,[password]] [/collection:TeamProjectCollectionUrl]
-*/
-
-var mapping_params = {
-    serverFolder: '$ֿֿֿֿ/',
-    localFolder: 'c:\\Ilan\\kaholo-plugin-tfs\\',
-    workspaceName: 'EC2AMAZ-15G1HG4',
-    URL: 'http://localhost:8080/tfs/DefaultCollection'
-}
-
-var label_params = {
-    labelName: 'ilan_label',
-    comment: '1234',
-    version: '',
-    scope: 'CosoleApplication1',
-    URL: 'http://localhost:8080/tfs/DefaultCollection'
-}
-
 function get(action) {
     /*
     This method is based on documentation of TFS Get: https://docs.microsoft.com/en-us/previous-versions/visualstudio/visual-studio-2010/fx7sdeyf(v=vs.100)
@@ -53,17 +29,7 @@ function get(action) {
 
         let tf = `cd ${action.params.workspacepath} && tf get ${flags.join(' ')}`
         console.log('Executing: ' + tf)
-        child_process.exec(tf, (error, stdout, stderr) => {
-            if (error) {
-                return reject(`exec error: ${error}`);
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                return reject(`exec error: ${stderr}`);
-            }
-            console.log(stdout)
-            return resolve(stdout);
-        });
+        executeCommand(tf)
     })
 }
 
@@ -77,10 +43,8 @@ function workfold(action,settings) {
             return reject("Missing server folder path")
         if (!action.params.localfolder)
             return reject("Missing local folder path")
-        
-        
         if (settings.collection || action.params.collection) {
-                let col = settings.collection || action.params.collection
+                let col = action.params.collection || settings.collection
                 flags.push(`/collection:${col}`)
         }
         if (action.params.workspacename)
@@ -90,42 +54,19 @@ function workfold(action,settings) {
             flags.push(`/noprompt`)
         if (action.params.login || settings.login) {
             let lin = action.params.login || settings.login
-            var psw;
-            if (action.params.password) {
-                psw = `"${action.params.password}"`
-            }
-            else if (settings.password) {
-                psw = `"${settings.password}"`
-            }
-            flags.push(`/login:${lin},${psw}`)
+            let psw = `”${action.params.password || settings.password}”`
+            psw ? flags.push(`/login:${lin},${psw}`) : flags.push(`/login:${lin}`)
         }
         let tf = `tf workfold ${flags.join(' ')}`
         console.log("executing:" + tf)
-        child_process.exec(tf, (error, stdout, stderr) => {
-            if (error) {
-                return reject(`exec error: ${error}`);
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                return reject(`exec error: ${stderr}`);
-            }
-            console.log(stdout)
-            return resolve(stdout);
-        });
+        executeCommand(tf).then(resolve).catch(reject)
     })
 }
 
-function createLabel(action) {
+function executeCommand (cmd) {
     return new Promise((resolve, reject) => {
-        let flags = [];
-        if (action.params.noprompt)
-            flags.push('/noprompt')
-        if (action.params.comment)
-            flags.push(`/comment: ${action.params.comment}`)
-
-        let tf = `tf label ${p.labelName} @${p.scope} /noprompt /comment:"${p.comment}" /collection:${p.URL}`
-        console.log('Executing: ' + tf)
-        child_process.exec(tf, (error, stdout, stderr) => {
+        console.log("executing:" + cmd)
+        child_process.exec(cmd, (error, stdout, stderr) => {
             if (error) {
                 return reject(`exec error: ${error}`);
             }
@@ -138,32 +79,8 @@ function createLabel(action) {
         });
     })
 }
-
-function workspace(action, settings) {
-    return new Promise((resolve, reject) => {
-        let flags = [];
-        let tf = `tf label ${p.labelName} /noprompt /recursive /collection:${p.URL}`
-        console.log('Executing: ' + tf)
-        child_process.exec(tf, (error, stdout, stderr) => {
-            if (error) {
-                return reject(`exec error: ${error}`);
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                return reject(`exec error: ${stderr}`);
-            }
-            console.log(stdout)
-            return resolve(stdout);
-        });
-    })
-}
-
-//changeMapping({params : mapping_params})
-//createLabel(label_params)
 
 module.exports = {
-    workspace: workspace,
-    createLabel: createLabel,
     workfold: workfold,
     get: get
 }
